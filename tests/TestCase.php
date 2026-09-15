38809f197d6286f84426ef8c32dbb30f0ca4c97c
carlos-dasilva <carlos-dasilva@hotmail.com>
2026-09-15 18:05:00 -0300

    Remove dependencia de assets Vite dos testes PHP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // O job de testes PHP não gera os assets usados pelas views, inclusive a página 404.
+        $this->withoutVite();
+    }
 }
